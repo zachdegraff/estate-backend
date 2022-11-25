19edbccd6de03c3f46578a0625989b2a93934940
ahmetdoner7 <donerahmet093@gmail.com>
2022-11-25 06:34:16 +0100

    doctest

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome To Real Haven Estate Container Running On Docker - Google Cloud run';
+    return 'Welcome To Real Haven Estate APi Running On Docker - Google Cloud run';
   }
 }
